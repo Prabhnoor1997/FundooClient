5dcba781f393d41a77cc5f5262a13547c71f10b7
Prabhnoor <prabhnoor.parry@gmail.com>
2023-06-02 17:16:45 -0400

    archive updated

--- src/app/components/archive/archive.component.ts
+++ src/app/components/archive/archive.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { AppService } from '../../services/app.service';
+import { DataServiceService } from '../../services/data-service.service';
 
 @Component({
   selector: 'app-archive',
   templateUrl: './archive.component.html',
   styleUrls: ['./archive.component.css']
 })
-export class ArchiveComponent {
+export class ArchiveComponent implements OnInit {
+  notes : any = []
+  constructor(private service: AppService, private data: DataServiceService) {}
+  
+  name1 = "Archive";
+  name2 = ""
+  @Input() view:boolean =false;
+
+
+  ngOnInit() {
+    this.data.currentMessage.subscribe(message => this.view = message)
+    this.readAll();
+  }
+
+  childStatusChanged(finished: boolean) {
+    if (finished){
+      this.readAll();
+    }
+  }
+
+  readAll() {
+    this.notes = [];
+    this.service.getRequest('readnote').subscribe((data: any) => {
+      data.data.forEach(element => {
+        if(element.isArchive == true)
+          this.notes.push(element)
+      });
+    })
+
+  }
+
 
 }
